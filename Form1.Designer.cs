91a47ca48d805745cde547bd46c835281d37092a
Dr. Suresh Ramasamy <sureshdr@gmail.com>
2018-05-22 23:46:21 +0800

    Initial dump

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace RWProcess
+{
+    partial class frmControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmControl));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.txtProcessing = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtConsignment = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "In Folder";
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Enabled = false;
+            this.txtFilename.Location = new System.Drawing.Point(62, 15);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.ReadOnly = true;
+            this.txtFilename.Size = new System.Drawing.Size(174, 20);
+            this.txtFilename.TabIndex = 1;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(252, 12);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(70, 22);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(15, 49);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(132, 44);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "&Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(185, 49);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(136, 44);
+            this.btnQuit.TabIndex = 4;
+            this.btnQuit.Text = "&Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStatus.ForeColor = System.Drawing.Color.DarkCyan;
+            this.txtStatus.Location = new System.Drawing.Point(17, 127);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtStatus.Size = new System.Drawing.Size(304, 189);
+            this.txtStatus.TabIndex = 5;
+            // 
+            // txtProcessing
+            // 
+            this.txtProcessing.Enabled = false;
+            this.txtProcessing.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProcessing.ForeColor = System.Drawing.Color.Tomato;
+            this.txtProcessing.Location = new System.Drawing.Point(81, 327);
+            this.txtProcessing.Name = "txtProcessing";
+            this.txtProcessing.ReadOnly = true;
+            this.txtProcessing.Size = new System.Drawing.Size(48, 22);
+            this.txtProcessing.TabIndex = 6;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.ForeColor = System.Drawing.Color.LimeGreen;
+            this.txtTotal.Location = new System.Drawing.Point(219, 327);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(48, 22);
+            this.txtTotal.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Processing";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Total";
+            // 
+            // txtConsignment
+            // 
+            this.txtConsignment.Location = new System.Drawing.Point(185, 101);
+            this.txtConsignment.Name = "txtConsignment";
+            this.txtConsignment.ReadOnly = true;
+            this.txtConsignment.Size = new System.Drawing.Size(136, 20);
+            this.txtConsignment.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Processing Consignment Number";
+            // 
+            // frmControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 357);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtConsignment);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtProcessing);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmControl";
+            this.Text = " RW-Assist";
+            this.Load += new System.EventHandler(this.frmControl_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.TextBox txtProcessing;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtConsignment;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection; 
+
+namespace RWProcess
+{
+    public partial class frmControl : Form
+    {
+        string[] files;
+
+        public frmControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            List<string> cleanedList = new List<string>();
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtFilename.Text = fbd.SelectedPath;
+                    files = Directory.GetFiles(fbd.SelectedPath.ToString());
+                    foreach(string filename in files)
+                    {
+                        if (Path.GetFileName(filename).StartsWith("Consignment") ||
+                            Path.GetFileName(filename).StartsWith("Copy of Consignment"))
+                        {
+                            AddStatus("Currently evaluating - " + filename);
+                            cleanedList.Add(filename);
+                        }
+                        else
+                            AddStatus("Not considering - " + filename);
+                    }
+                    txtTotal.Text = cleanedList.Count.ToString();
+                    files = cleanedList.ToArray();
+                    btnProcess.Enabled = true;
+                }
+            }
+
+            
+/*
+            // Displays an OpenFileDialog so the user can select a Cursor.  
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Excel Files|*.xls";
+            openFileDialog1.Title = "Select an Excel File";
+
+            // Show the Dialog.  
+            // If the user clicked OK in the dialog and  
+            // a .CUR file was selected, open it.  
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Assign the cursor in the Stream to the Form's Cursor property.  
+                //this.Cursor = new Cursor(openFileDialog1.OpenFile());
+                txtFilename.Text = openFileDialog1.FileName;
+            }   */
+        }
+
+        private void frmControl_Load(object sender, EventArgs e)
+        {
+            btnProcess.Enabled = false;
+            this.Text = this.Text +  " " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        private void AddStatus(String status)
+        {
+            txtStatus.Text = "[" + DateTime.Now + "] " + status + "\r\n" + txtStatus.Text;
+        }
+
+        private void ProcessFile(string filename)
+        {
+            string dirprefix = @"C:\RWProcess\";
+            string targetfilename = "test";
+            bool GotData = true;
+            int RunningNumber = 1;
+
+            string re1 = ".*?"; // Non-greedy match on filler
+            string re2 = "(\\d+)";  // Integer Number 1
+
+            if (!Directory.Exists(dirprefix))
+            {
+                Directory.CreateDirectory(dirprefix);
+            }
+
+            Regex r = new Regex(re1 + re2, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            Match m = r.Match(Path.GetFileName(filename));
+            if (m.Success)
+            {
+                targetfilename = "MEL Equipment Condition Report - Batch " + m.Groups[1].ToString();
+                txtConsignment.Text = m.Groups[1].ToString();
+            }
+            else
+            {
+                targetfilename = "test" + String.Format("{000}", RunningNumber++);
+            }
+
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(filename);
+            txtStatus.Text += "Opening spreadsheet...";
+            Excel.Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            AddStatus("Retrieving range of used cells within the worksheet");
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            for (int i = 3; i <= 15; i++)
+            {
+                xlWorksheet.Rows[2].Delete();
+            }
+            AddStatus("Unwanted top rows deleted!");
+
+            // Get bottom rows
+            int CurRow = 4;
+            while (GotData)
+            {
+                //MessageBox.Show("Current value is : " + xlRange[CurRow, 1].Value2);
+                if (String.IsNullOrEmpty(Convert.ToString(xlRange[CurRow, 1].Value2)))
+                {
+                    GotData = !GotData;
+                    AddStatus("Found Empty Row");
+                    CurRow += 2;
+                }
+                else
+                {
+
+                    AddStatus("skip row...");
+                    CurRow++;
+                }
+            }
+
+            for (int i = 12; i <= 27; i++)
+            {
+                xlWorksheet.Rows[CurRow].Delete();
+            }
+            AddStatus("Unwanted bottom rows deleted!");
+
+            Excel.Range UsedRange = xlWorksheet.Range["C1"];
+            UsedRange.EntireColumn.Insert(Excel.XlInsertShiftDirection.xlShiftToRight,
+                    Excel.XlInsertFormatOrigin.xlFormatFromRightOrBelow);
+            AddStatus("Add Column at C1");
+
+            UsedRange = xlWorksheet.Range["C4"];
+            UsedRange.Value = "Matching with RW Audit";
+            UsedRange.Interior.Color = Excel.XlRgbColor.rgbYellow;
+
+            UsedRange = xlWorksheet.Range["N4"];
+            UsedRange.Value = "Quoted Price (MYR)";
+            UsedRange.Interior.Color = Excel.XlRgbColor.rgbGray ;
+
+            UsedRange = xlWorksheet.Range["O4"];
+            UsedRange.Value = "Revised Price (MYR)";
+            UsedRange.Interior.Color = Excel.XlRgbColor.rgbYellow;
+
+            UsedRange = xlWorksheet.Range["N5", "O" + (CurRow - 3).ToString()];
+            UsedRange.NumberFormat = "#,###.00";
+
+            UsedRange = xlWorksheet.Range["O" + (CurRow - 2).ToString()];
+            UsedRange.Formula = "=SUM(O5:" + "O" + (CurRow - 3).ToString();
+
+            UsedRange = xlWorksheet.Range["M" + (CurRow-2).ToString()];
+            UsedRange.Value = "Total";
+            UsedRange.Font.Bold = true;
+
+            UsedRange = xlWorksheet.Range["M" + (CurRow - 2).ToString(), "O" + (CurRow - 2).ToString()];
+            foreach (Excel.Range cell in UsedRange.Cells)
+            {
+                cell.BorderAround2();
+            }
+
+            UsedRange = xlWorksheet.Range["N4", "Q" + (CurRow - 3).ToString()];
+            foreach(Excel.Range cell in UsedRange.Cells)
+            {
+                cell.BorderAround2();
+            }
+            //UsedRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThin, Excel.XlColorIndex.xlColorIndexAutomatic);
+            //Excel.Borders border = UsedRange.Borders;
+            //border
+
+
+            UsedRange = xlWorksheet.Range["P4"];
+            UsedRange.Value = "Remarks";
+            UsedRange.Interior.Color = Excel.XlRgbColor.rgbYellow;
+
+            UsedRange = xlWorksheet.Range["Q4"];
+            UsedRange.Value = "Physical Serial No";
+            UsedRange.Interior.Color = Excel.XlRgbColor.rgbYellow;
+            // MessageBox.Show("Peekaboo!", "Important Message", MessageBoxButtons.OK, MessageBoxIcon.Warning
+            //     );
+            AddStatus("Saving file: " + dirprefix + targetfilename);
+            xlWorkbook.SaveAs(dirprefix + targetfilename, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+            false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+            Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            AddStatus("Initiating garbage collection");
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            AddStatus("Release COM Objects");
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            AddStatus("Close and release Excel workbooks");
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            AddStatus("Quit Excel and release Excel objects");
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int FileCounter = 1; 
+
+            foreach(string file in files)
+            {
+                txtProcessing.Text = FileCounter++.ToString();
+                ProcessFile(file);               
+            }
+        }
+    }
+}
